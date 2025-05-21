509a746e38ba7122a3a5c42ee54c85f6c785e0d1
Asia K <asialakaygrady@gmail.com>
2025-05-21 03:56:49 +0000

    updated dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,13 @@
 import type {NextConfig} from 'next';
+// Import withSentryConfig as a named export
+import { withSentryConfig } from '@sentry/nextjs';
+import withGenkit from '@genkit-ai/next';
+
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -20,4 +29,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(
+    bundleAnalyzer(nextConfig)
+  );
